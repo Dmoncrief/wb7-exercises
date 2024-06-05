303407e90cc08153f26318fc5469a274d9901b21
Destiny Moncrief <D.moncrief4@gmail.com>
2024-06-05 10:10:23 -0400

    made changes

--- website1/scripts/index.js
+++ website1/scripts/index.js
@@ -1,21 +1,38 @@
-"use strict"
-
+"use strict";
 
 const hello = document.getElementById("hello");
-const input1 = document.getElementById("input1");
-const paragraphOutput = document.getElementById("paragraphOutput");
+const apiUrlListCategories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+const apiUrlMealsInCategoryBase = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
 
+const categoriesRow = document.getElementById("categoriesRow")
 
 window.onload = function(){
-    hello.innerHTMl = "Hello Class";
-    input1.oninput = onInput1Input
+   // hello.innerHTML = "Hello Class";
+
+
+    fetch(apiUrlListCategories)
+    .then( response => response.json())
+    .then( data => {
+        console.log(data);
+        data.categories.forEach(createCategoryDiv);
+       
+    });
+
 
 }
 
 
-function onInput1Input() {
-    paragraphOutput.InnerHTMl += "<br>";
-    paragraphOutput.innerHTML += input1.value;
 
+function createCategoryDiv(category){
+    let div = document.createElement("div");
+    div.className = "col";
+    categoriesRow.appendChild(div);
+
+    let a = document.createElement("a");
+    a.href = "mealsInCategory.html?category=" + category.strCategory;
+    a.innerHTML = category.strCategory;
+
+    div.appendChild(a);
+
+    console.log(category.strCategory);
 }
-
